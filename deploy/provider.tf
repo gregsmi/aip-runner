2367154c20c88c1c8a31a6ed9fc5eea10f3fab28
Greg Smith <gregsmi@microsoft.com>
2023-05-10 12:51:26 -0700

    added TF init

--- deploy/provider.tf
+++ deploy/provider.tf
@@ -2,13 +2,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.34.0"
+      version = "=3.55.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "=2.31.0"
+      version = "=2.38.0"
     }
   }
+}
+
+# Use Azure blob store to manage Terraform state - fill in 
+# required fields via -backend-config on terraform init.
+terraform {
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
